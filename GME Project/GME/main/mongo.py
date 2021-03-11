b9ebdc66f3124aabf8642f132d093470728e45c3
DockingMule <aca0824@gmail.com>
2021-03-11 16:44:48 -0500

    added function to mong.py

--- GME Project/GME/main/mongo.py
+++ GME Project/GME/main/mongo.py
@@ -5,78 +5,188 @@ from bson.objectid import ObjectId
 import pprint
 import sys
 
-print("Hello world")
 client = pymongo.MongoClient("mongodb+srv://admin:tothemoon@userdata.jbuat.mongodb.net/gme?retryWrites=true&w=majority")
 try:
-   # The ismaster command is cheap and does not require auth.
-   client.admin.command('ismaster')
-   print("Successfully connected to database")
+    # The ismaster command is cheap and does not require auth.
+    client.admin.command('ismaster')
+    print("Successfully connected to database")
 except ConnectionFailure:
-   print("Server not available")
+    print("Server not available")
 db = client.gme
 collection = db.users
-##{ 
-##   username: 'aca0824',
-##   displayname: 'AustinAlloway', 
-##	 spotify_username: 'aca0824',
-##	 Spotify Profile URL: 'idk it',
-##	 Access Token: 'no clue',
-##	 email: 'nothings',
-##	 profile_pic: 'https://i.scdn.co/image/ab6775700000ee859bf0698648aebb835bdb6412',
-##	 age: 21,
-##	 gender: 'Male',
-##	 country: 'New Jersey',
-##	 match_pref:
-##	 	{
-##	        	age_min: 19,
-##	                age_max: 25,
-##	                gender: [ 'Male', 'Female']
-##	         },      
+
+#####################################################################################
+#                                                                                   #
+#                        SAMPLE USER PROFILE FOR REFERENCE                          #
+#                       -----------------------------------                         #
+#                                                                                   #
+#####################################################################################
+#{ 
+#   username: 'aca0824',
+#   displayname: 'AustinAlloway', 
+#	 spotify_username: 'aca0824',
+#	 Spotify Profile URL: 'idk it',
+#	 Access Token: 'no clue',
+#	 email: 'nothings',
+#	 profile_pic: 'https://i.scdn.co/image/ab6775700000ee859bf0698648aebb835bdb6412',
+#	 age: 21,
+#	 gender: 'Male',
+#	 country: 'New Jersey',
+#	 match_pref:
+#	 	{
+#	        age_min: 19,
+#	        age_max: 25,
+#	        gender: [ 'Male', 'Female']
+#	    },      
 #	favorite_users: [ ],
 #	music_profile:
-#	       {
-#			acousticness: 0.5,
-#       		danceability: 0.5,
-#        		energy: 0.5,
-#			instrumentalness: 0.5,
-#			liveness: 0.5,
-#			loudness: 0.5,
-#			speechiness: 0.5,
-#			tempo: 0.5,
-#			time_signature: 0.5,
-#			valence: 0.5
+#	    {
+#	    	acousticness: 0.5,
+ #         	danceability: 0.5,
+  #       	energy: 0.5,
+	#    	instrumentalness: 0.5,
+	 #   	liveness: 0.5,
+	#		loudness: 0.5,
+	#		speechiness: 0.5,
+	#		tempo: 0.5,
+	#		time_signature: 0.5,
+	#		valence: 0.5
 #	      }
 #}
 
 
-# Get username from ObjectID !! requires ObjectID package from bson.objectid
-def get_username():
-    pprint.pprint(collection.find_one( {"_id": ObjectId('6047f27a3d015cd9b40ae346')}, {"username" : 1, "_id": 0}))
+#####################################################################################
+# Param: ObjectID of User                                                           #
+# Function: Takes user ObjectID and returns their username                          #
+# RETURNS: String username                                                          #
+# ON FAIL: Returns Falso                                                            #
+#####################################################################################
+def get_username(userid):
+    try:
+        return collection.find_one( {"_id": userid}, {"username" : 1, "_id": 0})
+    except:
+        return False
 
-# Update existing user's music profile
-def update_music_profile():
-    pprint.pprint(collection.update_many({"username": "aca0824"}, { '$set' : { "music_profile": {'danceability': 0.4,'energy': 0.5,'mode': 0.4,'speechiness': 0.5,'acousticness': 0.2,'instrumentalness': 0.7,'liveeness': 0.3,'valence': 0.5}}}))
+#####################################################################################
+# Param: username as String, newMusicProfile as Json                                #
+# Function: Overwrites existing music profile with inputted profile                 #
+# RETURNS: True                                                                     #
+# ON FAIL: Returns Falso                                                            #
+#####################################################################################
+def update_music_profile(username, newMusicProfile):
+    try:
+        collection.update_many({"username": username}, { '$set' : { "music_profile": newMusicProfile}})
+        return True
+    except:
+        return False
 
-# Print single user's profile from collection
+#####################################################################################
+# Param: none                                                                       #
+# Function: get random user profile                                                 #
+# RETURNS: user profile                                                             #
+# ON FAIL: Returns Falso                                                            #
+#####################################################################################
 def find_one():
-    pprint.pprint(collection.find_one())
+    try:
+        return(collection.find_one())
+    except:
+        return False
 
-    # get music profile
+#####################################################################################
+# Param: username of User                                                           #
+# Function: get user profile of denoted user                                        #
+# RETURNS: user profile                                                             #
+# ON FAIL: Returns Falso                                                            #
+#####################################################################################
+def find_user(user):
+    try:
+        return(collection.find_one({ 'username': user}))
+    except:
+        return False
 
-    # get user profile
+#####################################################################################
+# Param: username as String, new user profile as JSON                               #
+# Function: overwrite existing user profile with inputted one                       #
+# RETURNS: True                                                                     #
+# ON FAIL: Returns Falso                   !!!BORKED!!!                             #
+#####################################################################################
+def update_user():
+    try:
+        collection.update_many({'username': 'aca0824'}, { '$set' : { 
+   'username': 'aca0824',
+   'displayname': 'AustinAlloway', 
+	 'spotify_username': 'aca0824',
+	 'spotify_profile_url': 'idk it',
+	 'Access Token': 'no clue',
+	 'email': 'nothings',
+	 'profile_pic': 'https://i.scdn.co/image/ab6775700000ee859bf0698648aebb835bdb6412',
+	 'age': 21,
+	 'gender': 'Male',
+	 'country': 'New Jersey',
+	 'match_pref':
+	 	{
+	        'age_min': 19,
+	        'age_max': 25,
+	        'gender': [ 'Male', 'Female']
+	    },      
+	'favorite_users': [ ],
+	'music_profile':
+	    {
+	    	'acousticness': 0.5,
+          	'danceability': 0.5,
+         	'energy': 0.5,
+	    	'instrumentalness': 0.5,
+	    	'liveness': 0.5,
+			'loudness': 0.5,
+			'speechiness': 0.5,
+			'tempo': 0.5,
+			'time_signature': 0.5,
+			'valence': 0.5
+	      }
+})
+        return True
+    except:
+        return False
+    
 
-    # update music profile 
+#####################################################################################
+# Param: user name as String                                                        #
+# Function: Checks is given username is currently in use                            #
+# RETURNS: True (In use)                                                            #
+# ON FAIL: Returns Falso (Not in use)                                               #
+#####################################################################################
+def check_username(username):
+    if (collection.find_one({'username': username})):
+        return True
+    else:
+        return False
 
-    # update user profile
 
-
-
-## Execute certian functions based on command line arguments
-## No arguments just show successful or failed connection to database
-## example terminal command: $ python3 testDB.py 1
+#####################################################################################
+#  This is                                                                          #
+#                  Just For                                                         #
+#                                   TESTING                                         #
+#  Running in terminal                                    purposes                  #
+#####################################################################################
+## Execute certain functions based on command line arguments
+## No arguments prints only successful or failed connection to database
+## example terminal command: $ python3 mongo.py 1
 if len(sys.argv) > 1:
     if sys.argv[1] == '1':
-        find_one()
+        print(find_one())
 
     if sys.argv[1] == '2':
-        get_username()
+        print(get_username())
+
+    if sys.argv[1] == '3':
+        print(find_user(sys.argv[2]))
+
+    if sys.argv[1] == '4':
+        print(check_username(sys.argv[2]))
+    
+    if sys.argv[1] == '5':
+        print(update_user())
+else:
+    print("!!! No arguments given !!!")
+    print("Run mongo.py with arguments 1, 2, 3, 4, etc.")
+    print('example terminal command: $ python3 mongo.py 1')
