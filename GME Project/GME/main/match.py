93bdcb492da8884332dd2b09278438381d8948ef
Talia Tomarchio <70025062+taliatomarchio@users.noreply.github.com>
2021-03-30 02:22:12 -0400

    finished pref

--- GME Project/GME/main/match.py
+++ GME Project/GME/main/match.py
@@ -1,38 +1,40 @@
 #Match Algo
 import mongo
 
-user = mongo.find_user('k7lw')
-
-def match_pref(user):
-    prefs = user['match_pref']
+def match_pref(username_in_session):
+    user = mongo.find_user(username_in_session)
     arraylist_gender_matches = genderfind(user)
-    #agefind(arraylist_gender_matches)
-    return arraylist_gender_matches
+    return agefind(arraylist_gender_matches, user)
 
 
 def genderfind(user):
-    prefs = user['match_pref'][0]
+    prefs = user['match_pref']
     gender = prefs['gender']
     list_of_users = mongo.find_all()
     genderarray = []
     for user2 in list_of_users:
         if (user2['gender'] in gender):
             genderarray.append(user2)
-    return genderarray    
+    return genderarray
 
 
-def agefind(arraylist_gender_matches):
+def agefind(arraylist_gender_matches, user):
     prefs = user['match_pref']
     minage = prefs['age_min']
     maxage = prefs['age_max']
     list_of_users = arraylist_gender_matches
+    successfulmatchlist = [] 
     for user2 in list_of_users:
-        if (age_min < (user2)['age'] > age_max):
-            list_of_users.remove(user2)
-    return finalarray
+        if (minage < (user2)['age'] and (user2)['age'] < maxage):
+            successfulmatchlist.append(user2)
+    return successfulmatchlist
 
+test_username = "k7lw"
 
-print(match_pref(user))
-
-#return only display name
-#statement if no gender is chosen if else null
+for user in match_pref(test_username):
+    print(user['displayname'])
+    print(user['profile_pic'])
+    print(user['age'])
+    print(user['gender'])
+    print(user['country'] + "\n")
+    #% of matchability
