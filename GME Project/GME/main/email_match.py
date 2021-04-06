2bd74dfbcc44bf4d61d53e720b971fd7bd0e3691
Talia Tomarchio <70025062+taliatomarchio@users.noreply.github.com>
2021-04-06 00:39:24 -0400

    new email message

--- GME Project/GME/main/email_match.py
+++ GME Project/GME/main/email_match.py
@@ -4,13 +4,13 @@ def sendemail(to_email,user_displayname,user_email):
         password = "Diamondh4nd$"
         sender_email = "gme.matchmaking@gmail.com" # Enter your address
         
-        SUBJECT = 'NEW MATCH FROM GME MATCH MAKING!!!'
+        SUBJECT = 'YOU HAVE A NEW MATCH FROM GME!!'
         TEXT = """\
         Great News! You have been matched with {} and they have requested to communicate with you!
         Here is their email: {}.
-        We hope something blossoms and you conversations go great!
+        We hope something blossoms from your new found romance and you listen to music together.
         Sincerely,
-            GME Match Making.""".format(user_displayname,user_email)
+            GME Match Maker.""".format(user_displayname,user_email)
         message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
         conn = smtplib.SMTP('imap.gmail.com',587)
         conn.ehlo()
